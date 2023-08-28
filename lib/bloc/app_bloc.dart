cadf6b752d52ccf569b67f85d100e42f6fdc6a2d
Kerem Erdine <vloggerefsane2009@gmail.com>
2023-08-28 18:21:32 +0300

    Moved the on function to an external function.

--- lib/bloc/app_bloc.dart
+++ lib/bloc/app_bloc.dart
@@ -5,18 +5,22 @@ part 'app_event.dart';
 part 'app_state.dart';
 
 class AppBloc extends Bloc<AppEvent, AppState> {
+  AppState questionAnswered(String selectedAnswer) {
+    if (state.selectedAnswers!.length == questions.length - 1) {
+      return state.copyWith(
+          selectedAnswers: state.copyAnswersWith(selectedAnswer),
+          activeScreen: 'results-screen');
+    } else {
+      return state.copyWith(
+          currentQuestionIndex: state.currentQuestionIndex =
+              state.currentQuestionIndex! + 1,
+          selectedAnswers: state.copyAnswersWith(selectedAnswer));
+    }
+  }
+
   AppBloc() : super(AppState()) {
     on<AppQuestionAnswerButtonPressed>((event, emit) {
-      if (state.selectedAnswers!.length == questions.length - 1) {
-        emit(state.copyWith(
-            selectedAnswers: state.copyAnswersWith(event.selectedAnswer),
-            activeScreen: 'results-screen'));
-      } else {
-        emit(state.copyWith(
-            currentQuestionIndex: state.currentQuestionIndex =
-                state.currentQuestionIndex! + 1,
-            selectedAnswers: state.copyAnswersWith(event.selectedAnswer)));
-      }
+      emit(questionAnswered(event.selectedAnswer));
     });
     on<AppQuizStarted>((event, emit) {
       emit(state.copyWith(
