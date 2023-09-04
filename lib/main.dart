7af09a62de6d4c1142c12fe6c524758d870216aa
Kerem Erdine <vloggerefsane2009@gmail.com>
2023-09-04 16:48:41 +0300

    modified:   lib/main.dart 	deleted:    lib/quiz.dart 	lib/screens/quiz.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:adv_basics/bloc/app_bloc.dart';
 import 'package:flutter/material.dart';
 
-import 'package:adv_basics/quiz.dart';
+import 'package:adv_basics/screens/quiz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
